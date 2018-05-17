b0a72905b1623171d076b72cd708d287a9e80f68
Li YANG <yang-li@tcl.com>
2018-05-17 16:11:17 +0800

    m

--- app/src/main/java/com/example/yangli/test1/Fragment1.java
+++ app/src/main/java/com/example/yangli/test1/Fragment1.java
@@ -2,31 +2,54 @@ package com.example.yangli.test1;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-
-import java.util.zip.Inflater;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 /**
  * Created by yangli on 2018/5/16.
  */
 
-public class Fragment1 extends Fragment implements View.OnClickListener {
-    private EditText editText;
+public class Fragment1 extends Fragment implements View.OnClickListener,RadioGroup.OnCheckedChangeListener{
+    private RadioGroup radioGroup;
+    private RadioButton r1;
+    private RadioButton r2;
+    private RadioButton r3;
+    private RadioButton r4;
+    private RadioButton r5;
+    private RadioButton r6;
+    private RadioButton r7;
+    private RadioButton r8;
+    private RadioButton r9;
     private Button button;
     private Submit submit;
+    private String text = null;
+    private SharedPreferences sp;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment1, container, false);
-        editText = view.findViewById(R.id.f1_text_1);
         button = view.findViewById(R.id.f1_but1);
         button.setOnClickListener(this);
+        radioGroup = view.findViewById(R.id.radiogroup);
+        radioGroup.setOnCheckedChangeListener(this);
+        r1 = view.findViewById(R.id.r1);
+        r2 = view.findViewById(R.id.r2);
+        r3 = view.findViewById(R.id.r3);
+        r4 = view.findViewById(R.id.r4);
+        r5 = view.findViewById(R.id.r5);
+        r6 = view.findViewById(R.id.r6);
+        r7 = view.findViewById(R.id.r7);
+        r8 = view.findViewById(R.id.r8);
+        r9 = view.findViewById(R.id.r9);
+        sp = getContext().getSharedPreferences("User", Context.MODE_PRIVATE);
+        setCheck();
         return view;
     }
 
@@ -35,9 +58,48 @@ public class Fragment1 extends Fragment implements View.OnClickListener {
         switch (v.getId()) {
             case R.id.f1_but1:
                 submit = (Submit)getActivity();
-                submit.submit(editText.getText().toString());
+                submit.submit(text);
                 break;
         }
     }
 
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putInt("check",checkedId);
+        editor.commit();
+        switch (checkedId){
+            case R.id.r1:
+                text = "这是1";
+                break;
+            case R.id.r2:
+                text = "这是2";
+                break;
+            case R.id.r3:
+                text = "这是3";
+                break;
+            case R.id.r4:
+                text = "这是4";
+                break;
+            case R.id.r5:
+                text = "这是5";
+                break;
+            case R.id.r6:
+                text = "这是6";
+                break;
+            case R.id.r7:
+                text = "这是7";
+                break;
+            case R.id.r8:
+                text = "这是8";
+                break;
+            case R.id.r9:
+                text = "这是9";
+                break;
+        }
+    }
+    private void setCheck(){
+       int id = sp.getInt("check",R.id.r1);
+       radioGroup.check(id);
+    }
 }
